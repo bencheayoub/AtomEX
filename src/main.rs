dbd924bfa6cab8a09f177580e5f2b02a10005993
bencheayoub <AyoubBenChetioui@gmail.com>
2026-07-10 21:34:33 +0100

    add the function to read the file path.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io;
 
-fn file_path(){
+fn file_path() -> String
+{
+    println!("Enter the file path: ");
     let mut path = String::new();
     io::stdin().read_line(&mut path).expect("Failed to read line");
-    printls!("File path: {}", path);
-
+    println!("The file path is: {}", path);
+    return path;
 }
 
 
 fn main(){
-
+file_path();
 }
